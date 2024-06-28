32429d55c7be05544b8c51a1a15109fec1b26921
UCHIYAMA Ryota <lessironglance@gmail.com>
2024-06-28 11:10:03 +0900

    add auth samples

---
+++ frontend/app/services/auth.server.ts
@@ -0,0 +1,7 @@
+import { Authenticator } from "remix-auth";
+import { sessionStorage } from "~/services/session.server";
+
+// Create an instance of the authenticator, pass a generic with what
+// strategies will return and will store in the session
+// TODO: replace `string` with the type that your strategy returns
+export let authenticator = new Authenticator<string>(sessionStorage);
