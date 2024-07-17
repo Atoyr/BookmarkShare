8058ed1e8bb979c38351b10e44325ef0b25603be
UCHIYAMA Ryota <lessironglance@gmail.com>
2024-07-17 20:13:04 +0900

    add form

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -3,7 +3,19 @@ import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { authenticator } from "~/services.server/auth";
 import { FormStrategy } from "remix-auth-form";
 
-import { LoginForm } from "~/components";
+import { Link } from "@remix-run/react";
+import { Form } from "@remix-run/react";
+
+import { Button } from "~/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card"
+import { Input } from "~/components/ui/input"
+import { Label } from "~/components/ui/label"
 
 
 export const meta: MetaFunction = () => {
@@ -16,7 +28,50 @@ export const meta: MetaFunction = () => {
 export default function Dashboard() {
   return (
     <div className="font-sans p-4">
-    <LoginForm />
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form method="post">
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <Link to="/">
+                    Forgot your password?
+                  </Link>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <Button variant="outline" className="w-full">
+                Login with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link to="/">
+                Sign up
+              </Link>
+            </div>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
@@ -27,6 +82,25 @@ export async function action({ request }: ActionFunctionArgs) {
   // we call the method with the name of the strategy we want to use and the
   // request object, optionally we pass an object with the URLs we want the user
   // to be redirected to after a success or a failure
+  console.log("action!")
+  //
+  //
+  authenticator.use(
+    new FormStrategy(async ({ form}) => {
+      let email = form.get("email");
+      let password = form.get("password");
+      console.log(email);
+      console.log(password);
+
+      // the type of this user must match the type you pass to the Authenticator
+      // the strategy will automatically inherit the type if you instantiate
+      // directly inside the `use` method
+      return "";
+    }),
+    // each strategy has a name and can be changed to use another one
+    // same strategy multiple times, especially useful for the OAuth2 strategy.
+    "user-pass"
+  );
   return await authenticator.authenticate("user-pass", request, {
     successRedirect: "/dashboard",
     failureRedirect: "/login",
@@ -37,8 +111,8 @@ export async function action({ request }: ActionFunctionArgs) {
 // authenticated with `authenticator.isAuthenticated` and redirect to the
 // dashboard if it is or return null if it's not
 export async function loader({ request }: LoaderFunctionArgs) {
+
+  console.log("loader!")
   // If the user is already authenticated redirect to /dashboard directly
-  return await authenticator.isAuthenticated(request, {
-    successRedirect: "/dashboard",
-  });
+  return await authenticator.isAuthenticated(request, { successRedirect: "/dashboard", });
 };
