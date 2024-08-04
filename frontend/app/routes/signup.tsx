8028eef3588ffffb50590682317832719396eb9e
UCHIYAMA Ryota <lessironglance@gmail.com>
2024-08-04 21:28:12 +0900

    add supabase and auth

--- frontend/app/routes/signup.tsx
+++ frontend/app/routes/signup.tsx
@@ -1,21 +1,100 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { Form, Link } from "@remix-run/react";
+import { supabase } from "~/lib/supabase";
 import { Button } from "~/components/ui/button";
-import { SignupForm } from "~/components";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card"
+import { Input } from "~/components/ui/input"
+import { Label } from "~/components/ui/label"
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "signup" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
-export default function Dashboard() {
+// TODO use Form
+function SignupForm() {
+  return (
+    <Form method="post">
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardDescription>
+          Signup
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link to="/">
+                Forgot your password?
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link to="/">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+    </Form>
+  )
+}
+
+export async function action({
+  request,
+}: ActionFunctionArgs) {
+  const body = await request.formData();
+  const user = {
+//    email: body.get("email") as string,
+//    password: body.get("pasword") as string,
+    email:'sample@gmail.com',
+    password: 'password',
+  };
+
+  const { data, error } = await supabase.auth.signUp(user);
+
+  if (error) {
+    console.log(error);
+  }
+
+  console.log(data);
+  return data;
+}
+
+
+export default function Signup() {
   return (
     <div className="font-sans p-4">
     <SignupForm />
     </div>
   );
 }
-
-
-
--- frontend/app/services.server/auth/auth.ts
+++ frontend/app/services.server/auth/auth.ts
@@ -1,7 +1,41 @@
-import { Authenticator } from "remix-auth";
+import { Authenticator, AuthorizationError } from "remix-auth";
+import { FormStrategy } from 'remix-auth-form'
+
 import { sessionStorage } from "~/services.server/auth/session";
+
 
 // Create an instance of the authenticator, pass a generic with what
 // strategies will return and will store in the session
 // TODO: replace `string` with the type that your strategy returns
-export let authenticator = new Authenticator<string>(sessionStorage);
+const authenticator = new Authenticator<string>(sessionStorage);
+
+const formStrategy = new FormStrategy(async ({ form }) => {
+  const email = form.get('email')
+  const password = form.get('password')
+
+  if (!(email && password)) {
+    throw new Error('Invalid Request')
+  }
+
+  // TODO GET USER
+  //const user = await prisma.user.findUnique({ where: { email: String(email) } })
+
+  if (!user) {
+    throw new AuthorizationError()
+  }
+
+  // TODO PASSWORD MATCH
+  //const passwordsMatch = await bcrypt.compare(String(password), user.password)
+
+  if (!passwordsMatch) {
+    throw new AuthorizationError()
+  }
+
+  const { password: _, ...userWithoutPassword } = user
+
+  return userWithoutPassword
+})
+
+authenticator.use(formStrategy, 'user-pass')
+
+export { authenticator }
