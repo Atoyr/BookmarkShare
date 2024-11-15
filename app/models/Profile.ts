4dc4bba67888ade112abbeee97068a3565755980
UCHIYAMA Ryota <lessironglance@gmail.com>
2024-11-15 20:46:21 +0900

    update: add avater url

--- app/models/Profile.ts
+++ app/models/Profile.ts
@@ -3,10 +3,12 @@ export interface Profile {
   username: string;
   email: string;
   user_id: string;
+  avater_url: string;
 }
 
 export interface ProfileInput {
   username: string;
   email: string;
   user_id: string;
+  avater_url: string;
 }
