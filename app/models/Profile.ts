7e22ae7950340958959748d073a746e746594950
UCHIYAMA Ryota <lessironglance@gmail.com>
2024-11-16 18:08:11 +0900

    update: model and client

--- app/models/Profile.ts
+++ app/models/Profile.ts
@@ -1,15 +1,12 @@
-// models/Profile.ts
-export interface Profile {
-  id: number;
+import { Model } from './Model';
+
+export interface ProfileInput extends Model {
   username: string;
   email: string;
   user_id: string;
   avatar_url?: string;
+}
+export interface Profile extends ProfileInput {
+  id: number;
 }
 
-export interface ProfileInput {
-  username: string;
-  email: string;
-  user_id: string;
-  avatar_url?: string;
-}
--- app/utils/supabase/client.ts
+++ app/utils/supabase/client.ts
@@ -1,6 +1,11 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-export function createClient({ supabaseUrl, supabaseAnonKey }: { supabaseUrl: string | null, supabaseAnonKey: string | null}) {
+export interface SupabaseClientArgs {
+  supabaseUrl: string | null, 
+  supabaseAnonKey: string | null
+}
+
+export function createClient({ supabaseUrl, supabaseAnonKey }: SupabaseClientArgs) {
   if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error('Missing Supabase environment variables');
   }
@@ -9,4 +14,3 @@ export function createClient({ supabaseUrl, supabaseAnonKey }: { supabaseUrl: st
     supabaseAnonKey!
   )
 };
-
