c25d4a78aef20ffde72793cdd81b309dc1375244
UCHIYAMA Ryota <lessironglance@gmail.com>
2024-11-26 23:45:06 +0900

    update: filter

--- app/routes/_app.s_.$space_id_.bg_.$bookmark_group_id/route.tsx
+++ app/routes/_app.s_.$space_id_.bg_.$bookmark_group_id/route.tsx
@@ -1,13 +1,18 @@
 "use client"
+import { useState } from "react";
 import { useLoaderData } from "@remix-run/react";
 import { type MetaFunction, type LoaderFunction } from "@remix-run/node";
+
+import { Button } from "~/components/ui/button"
+import { Input } from "~/components/ui/input"
+import { Plus } from "lucide-react"
 
 import { getBookmarkGroupById, getBookmarksByBookmarkGroupId } from "~/services/Bookmark.server";
 import { Bookmark } from "~/models";
 
 import { DataTable } from "./DataTable";
 import { columns } from "./columns";
-import { space } from "postcss/lib/list";
+import { NewDialog } from "./NewDialog"
 
 export const meta: MetaFunction = () => {
   return [
@@ -37,10 +42,31 @@ export let loader: LoaderFunction = async ({request, params}) => {
 
 export default function bookmarks() {
   const { title, bookmarks, spaceId, bookmarkGroupId }= useLoaderData<{title: string, bookmarks: Bookmark[], spaceId: string, bookmarkGroupId: string}>();
+  const [filterValue, setFilterValue] = useState("");  
+
+  const filteredData = bookmarks.filter((row) =>
+    row.title.toLowerCase().includes(filterValue.toLowerCase())
+  );
+  const handleFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterValue(e.target.value)
+  }
 
   return (
     <div className="m-16 flex gap-2 flex-col">
-      <DataTable columns={columns} data={bookmarks} spaceId={spaceId} bookmarkGroupId={bookmarkGroupId}/>
+      <div className="flex flex-col gap-2">
+        <div className="flex">
+          <div className="flex-1">
+            <Input
+              placeholder="Filter title..."
+              value={filterValue}
+              onChange={handleFilterChange}
+              className="max-w-sm"
+            />
+          </div>
+          <div className="flex-none"><NewDialog spaceId={spaceId} bookmarkGroupId={bookmarkGroupId}/></div>
+        </div>
+        <DataTable columns={columns} data={filteredData} spaceId={spaceId} bookmarkGroupId={bookmarkGroupId}/>
+      </div>
     </div>
   );
 }
